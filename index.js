8e61e23178d3195dd5608c04d02d15bff39b4e9b
Tobi Balogun <tbalogun146@gmail.com>
2022-10-17 08:57:38 +0100

    practicing java script

---
+++ index.js
@@ -0,0 +1,146 @@
+// function isLandscape (width, height) {
+//    return (width > height) ? true : false;
+// }
+// console.log(isLandscape(200,100));
+
+// function Fizzbuzz() {
+//     for (let i=0; i <= 100; ++1)
+//       if (i % 3 === 0 && i % 5 === 0) {
+//         console.log("fizzbuzz")
+//       }
+// }
+
+let food = {
+    name : 'tobi'
+}
+
+
+
+// // food['name'] = 'amala';
+// // console.log(food['name'])
+
+// function takeCare() {
+
+// }
+
+// let fish = {
+//     time : 'clock',
+//     money : 30
+// }
+// fish.time = 'gala'
+// fish['time'] = 'sweet'
+
+// fish.money = 50
+
+// console.log(fish.time, fish.money) 
+
+// let amala = ['eba','garri','kilishi']
+
+// console.log(amala.length)
+
+// function greet (baba) {
+//  console.log ('hello'   , baba)
+
+// }
+// greet('tobi')
+
+// function greet(firstName , lastName); {
+//     console.log('hello' + firstName + lastName)
+// }
+// greet('tobi', 'tola');
+
+// function square(number) {
+//    console.log(number * number)
+// }
+// square(2)
+
+
+// let points = 10;
+
+// let customerType = points > 100 ? 'gold' : 'silver'
+// console.log (customerType);
+
+
+// let highIncome = true;
+// let goodCreditScore = true;
+
+// let eligibleForLoan = highIncome && goodCreditScore
+
+// console.log('you are' + eligibleForLoan)
+
+
+//  let highIncome = false;
+// let goodCreditScore = false;
+
+// let eligibleForLoan = highIncome || goodCreditScore
+
+// console.log('you are' + eligibleForLoan)
+
+
+// let a = 'red';
+// let b = 'blue';
+
+// console.log(a,b)
+
+// let c = a;
+// let a = b;
+// let b = c;
+
+
+
+// // console.log(a,b,)
+// let pi = 3.14
+// let radius = 7
+// let area = pi * radius * radius
+// // console.log('the area is', + area)
+
+// let hour = 15;
+
+//  if(hour >= 6 && hour <12) {
+//     console.log('good morning tobi')
+//  }
+//  else if(hour >= 12 && hour <24) {
+//     console.log('good afternoon tobi')
+//  }
+
+
+// switchcase
+// let role; 
+// switch(role){
+//     case 'guest':
+//         console.log('guest user')
+// }
+
+// LOOPS.
+
+
+
+// for (let i = 0; i <= 10; i++){
+// console.log(i)
+// }
+
+// WHILE LOOP
+// let i = 0;
+// while(i < 1000000){
+//     console.log(i)
+//     i++
+// }
+// DO WHILE LOOP
+// let i = 0;
+// do{console.log(i);
+
+// }
+
+// function maximumOfTwoNumbers (num1, num2) { 
+//   return num1 > num2 ? num1 : num2
+//   }
+
+//  console.log (maximumOfTwoNumbers(25, 30))
+
+function isLandscape (width, height){
+    return width > height ? true : false 
+
+}
+
+
+  console.log (isLandscape(200, 500))
